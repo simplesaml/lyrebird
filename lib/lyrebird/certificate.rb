1dbf56a054b66371c070f4a82f9066c921955aaa
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-23 09:14:01 -0500

    Add self-signed certificate generation

--- lib/lyrebird/certificate.rb
+++ lib/lyrebird/certificate.rb
@@ -2,7 +2,7 @@
 
 module Lyrebird
   class Certificate
-    attr_reader :private_key
+    attr_reader :private_key, :certificate
 
     def self.generate(bits: 2048)
       new(OpenSSL::PKey::RSA.new(bits))
@@ -10,6 +10,18 @@ module Lyrebird
 
     def initialize(private_key)
       @private_key = private_key
+      @certificate = build_certificate
+    end
+
+    private
+
+    def build_certificate
+      OpenSSL::X509::Certificate.new.tap do |c|
+        c.public_key = @private_key.public_key
+        c.not_before = Time.now
+        c.not_after = Time.now + (365 * 24 * 60 * 60)
+        c.sign(@private_key, OpenSSL::Digest::SHA256.new)
+      end
     end
   end
 end
--- test/lyrebird/certificate_test.rb
+++ test/lyrebird/certificate_test.rb
@@ -18,5 +18,26 @@ module Lyrebird
       certificate = Certificate.generate(bits: 4096)
       assert_equal 4096, certificate.private_key.n.num_bits
     end
+
+    def test_certificate_is_x509
+      certificate = Certificate.generate
+      assert_instance_of OpenSSL::X509::Certificate, certificate.certificate
+    end
+
+    def test_certificate_not_before_is_now
+      certificate = Certificate.generate
+      assert_in_delta Time.now, certificate.certificate.not_before, 1
+    end
+
+    def test_certificate_not_after_is_one_year_from_now
+      certificate = Certificate.generate
+      one_year = 365 * 24 * 60 * 60
+      assert_in_delta Time.now + one_year, certificate.certificate.not_after, 1
+    end
+
+    def test_certificate_is_signed
+      certificate = Certificate.generate
+      assert certificate.certificate.verify(certificate.private_key)
+    end
   end
 end
