eb030176da350382477857559119466dac77dc5e
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-23 19:06:56 -0500

    Add SAML namespace constants

--- lib/lyrebird.rb
+++ lib/lyrebird.rb
@@ -5,6 +5,7 @@ require "openssl"
 require "rexml"
 
 require_relative "lyrebird/certificate"
+require_relative "lyrebird/namespaces"
 require_relative "lyrebird/version"
 
 module Lyrebird
---
+++ lib/lyrebird/namespaces.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Lyrebird
+  SAML_ASSERTION_NS = "urn:oasis:names:tc:SAML:2.0:assertion"
+  SAML_PROTOCOL_NS = "urn:oasis:names:tc:SAML:2.0:protocol"
+  XMLDSIG_NS = "http://www.w3.org/2000/09/xmldsig#"
+end
