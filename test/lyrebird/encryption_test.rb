aae2a38ba4223e59514a506bbac0b3299fa71b31
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-25 13:38:26 -0500

    Clean up EncryptionTest

--- test/lyrebird/encryption_test.rb
+++ test/lyrebird/encryption_test.rb
@@ -9,6 +9,8 @@ module Lyrebird
       @element = @assertion.root
       @certificate = Certificate.generate
       @encrypted = Encryption.new(@element, @certificate).encrypt!
+      @ed = @encrypted.elements["xenc:EncryptedData"]
+      @cd = @ed.elements["xenc:CipherData"]
     end
 
     def test_returns_encrypted_assertion_element
@@ -21,52 +23,48 @@ module Lyrebird
     end
 
     def test_encrypted_data_element
-      ed = @encrypted.elements["xenc:EncryptedData"]
-      assert_equal "EncryptedData", ed.name
-      assert_equal "xenc", ed.prefix
+      assert_equal "EncryptedData", @ed.name
+      assert_equal "xenc", @ed.prefix
     end
 
     def test_encrypted_data_namespace
-      ed = @encrypted.elements["xenc:EncryptedData"]
-      assert_equal XMLENC_NS, ed.namespace
+      assert_equal XMLENC_NS, @ed.namespace
     end
 
     def test_encrypted_data_type
-      ed = @encrypted.elements["xenc:EncryptedData"]
-      assert_equal "#{XMLENC_NS}Element", ed.attributes["Type"]
+      assert_equal "#{XMLENC_NS}Element", @ed.attributes["Type"]
     end
 
     def test_encryption_method_element
-      em = @encrypted.elements["xenc:EncryptedData/xenc:EncryptionMethod"]
+      em = @ed.elements["xenc:EncryptionMethod"]
       assert_equal "EncryptionMethod", em.name
       assert_equal "xenc", em.prefix
     end
 
     def test_encryption_method_algorithm
-      em = @encrypted.elements["xenc:EncryptedData/xenc:EncryptionMethod"]
+      em = @ed.elements["xenc:EncryptionMethod"]
       assert_equal AES256_CBC, em.attributes["Algorithm"]
     end
 
     def test_cipher_data_element
-      cd = @encrypted.elements["xenc:EncryptedData/xenc:CipherData"]
-      assert_equal "CipherData", cd.name
-      assert_equal "xenc", cd.prefix
+      assert_equal "CipherData", @cd.name
+      assert_equal "xenc", @cd.prefix
     end
 
     def test_cipher_value_element
-      cv = @encrypted.elements["xenc:EncryptedData/xenc:CipherData/xenc:CipherValue"]
+      cv = @cd.elements["xenc:CipherValue"]
       assert_equal "CipherValue", cv.name
       assert_equal "xenc", cv.prefix
     end
 
     def test_cipher_value_is_base64
-      cv = @encrypted.elements["xenc:EncryptedData/xenc:CipherData/xenc:CipherValue"]
+      cv = @cd.elements["xenc:CipherValue"]
       decoded = Base64.strict_decode64(cv.text)
       assert decoded.bytesize > 16
     end
 
     def test_cipher_value_starts_with_iv
-      cv = @encrypted.elements["xenc:EncryptedData/xenc:CipherData/xenc:CipherValue"]
+      cv = @cd.elements["xenc:CipherValue"]
       decoded = Base64.strict_decode64(cv.text)
       iv = decoded[0, 16]
       assert_equal 16, iv.bytesize
