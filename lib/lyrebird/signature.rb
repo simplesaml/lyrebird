0c2c2fc901134e9b40618f970b90205a655e7e6b
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-28 09:05:04 -0500

    Convert Signature to Nokogiri

--- lib/lyrebird/signature.rb
+++ lib/lyrebird/signature.rb
@@ -2,73 +2,136 @@
 
 module Lyrebird
   class Signature
+    C14N_EXCLUSIVE = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
+
     def initialize(element, certificate)
       @element = element
       @certificate = certificate
+      @doc = element.document
     end
 
     def sign!
-      issuer = @element.elements["saml:Issuer"]
-      @element.insert_after(issuer, signature_element)
+      issuer = @element.at_xpath("saml:Issuer", "saml" => SAML_ASSERTION_NS)
+      issuer.add_next_sibling(build_signature)
       self
     end
 
     private
 
-    def signature_element
-      REXML::Element.new("ds:Signature").tap do |sig|
-        sig.add_namespace("ds", XMLDSIG_NS)
-        sig.add_element(signed_info)
-        sig.add_element(signature_value)
-        sig.add_element(key_info)
+    def build_signature
+      @doc.create_element("Signature").tap do |sig|
+        @ds = sig.add_namespace_definition("ds", XMLDSIG_NS)
+        sig.namespace = @ds
+
+        sig.add_child(build_signed_info)
+        sig.add_child(build_signature_value)
+        sig.add_child(build_key_info)
       end
     end
 
-    def signed_info
-      REXML::Element.new("ds:SignedInfo").tap do |si|
-        cm = si.add_element("ds:CanonicalizationMethod")
-        cm.add_attribute("Algorithm", EXC_C14N)
-        sm = si.add_element("ds:SignatureMethod")
-        sm.add_attribute("Algorithm", RSA_SHA256)
-        si.add_element(reference)
+    def build_signed_info
+      @doc.create_element("SignedInfo").tap do |si|
+        @signed_info = si
+        si.add_namespace_definition("ds", XMLDSIG_NS)
+        si.namespace = @ds
+
+        c14n_method = @doc.create_element("CanonicalizationMethod").tap do |cm|
+          cm.namespace = @ds
+          cm["Algorithm"] = EXC_C14N
+        end
+
+        sig_method = @doc.create_element("SignatureMethod").tap do |sm|
+          sm.namespace = @ds
+          sm["Algorithm"] = RSA_SHA256
+        end
+
+        si.add_child(c14n_method)
+        si.add_child(sig_method)
+        si.add_child(build_reference)
       end
     end
 
-    def signature_value
-      REXML::Element.new("ds:SignatureValue").tap do |sv|
-        sig = @certificate.sign(signed_info.to_s)
-        sv.text = Base64.strict_encode64(sig)
+    def build_signature_value
+      @doc.create_element("SignatureValue").tap do |sv|
+        sv.namespace = @ds
+        canonical = canonicalize_signed_info
+        sig = @certificate.sign(canonical)
+        sv.content = Base64.strict_encode64(sig)
       end
     end
 
-    def key_info
-      REXML::Element.new("ds:KeyInfo").tap do |ki|
-        x = ki.add_element("ds:X509Data")
-        x.add_element("ds:X509Certificate").text = @certificate.base64
+    def canonicalize_signed_info
+      save_opts = Nokogiri::XML::Node::SaveOptions::AS_XML
+      xml = @signed_info.to_xml(save_with: save_opts)
+      pattern = "<ds:SignedInfo>"
+      replacement = "<ds:SignedInfo xmlns:ds=\"#{XMLDSIG_NS}\">"
+      xml = xml.sub(pattern, replacement)
+      Nokogiri::XML(xml).root.canonicalize(C14N_EXCLUSIVE)
+    end
+
+    def build_key_info
+      @doc.create_element("KeyInfo").tap do |ki|
+        ki.namespace = @ds
+
+        x509_data = @doc.create_element("X509Data").tap do |xd|
+          xd.namespace = @ds
+
+          x509_cert = @doc.create_element("X509Certificate").tap do |xc|
+            xc.namespace = @ds
+            xc.content = @certificate.base64
+          end
+
+          xd.add_child(x509_cert)
+        end
+
+        ki.add_child(x509_data)
       end
     end
 
-    def reference
-      REXML::Element.new("ds:Reference").tap do |ref|
-        ref.add_attribute("URI", "##{@element.attributes["ID"]}")
-        ref.add_element(transforms)
-        dm = ref.add_element("ds:DigestMethod")
-        dm.add_attribute("Algorithm", SHA256_DIGEST)
-        ref.add_element("ds:DigestValue").text = compute_digest(@element)
+    def build_reference
+      @doc.create_element("Reference").tap do |ref|
+        ref.namespace = @ds
+        ref["URI"] = "##{@element["ID"]}"
+
+        ref.add_child(build_transforms)
+
+        digest_method = @doc.create_element("DigestMethod").tap do |dm|
+          dm.namespace = @ds
+          dm["Algorithm"] = SHA256_DIGEST
+        end
+
+        digest_value = @doc.create_element("DigestValue").tap do |dv|
+          dv.namespace = @ds
+          dv.content = compute_digest
+        end
+
+        ref.add_child(digest_method)
+        ref.add_child(digest_value)
       end
     end
 
-    def transforms
-      REXML::Element.new("ds:Transforms").tap do |t|
-        enveloped = t.add_element("ds:Transform")
-        enveloped.add_attribute("Algorithm", ENVELOPED_SIG)
-        c14n = t.add_element("ds:Transform")
-        c14n.add_attribute("Algorithm", EXC_C14N)
+    def build_transforms
+      @doc.create_element("Transforms").tap do |t|
+        t.namespace = @ds
+
+        enveloped = @doc.create_element("Transform").tap do |tr|
+          tr.namespace = @ds
+          tr["Algorithm"] = ENVELOPED_SIG
+        end
+
+        c14n = @doc.create_element("Transform").tap do |tr|
+          tr.namespace = @ds
+          tr["Algorithm"] = EXC_C14N
+        end
+
+        t.add_child(enveloped)
+        t.add_child(c14n)
       end
     end
 
-    def compute_digest(element)
-      digest = OpenSSL::Digest::SHA256.digest(element.to_s)
+    def compute_digest
+      canonical = @element.canonicalize(C14N_EXCLUSIVE)
+      digest = OpenSSL::Digest::SHA256.digest(canonical)
       Base64.strict_encode64(digest)
     end
   end
--- test/lyrebird/signature_test.rb
+++ test/lyrebird/signature_test.rb
@@ -4,18 +4,20 @@ require "test_helper"
 
 module Lyrebird
   class SignatureTest < Minitest::Test
+    NS = { "ds" => XMLDSIG_NS, "saml" => SAML_ASSERTION_NS }.freeze
+
     def setup
       @certificate = Certificate.build
       @assertion = Assertion.new.document
       @element = @assertion.root
       Signature.new(@element, @certificate).sign!
-      @signature = @element.elements["ds:Signature"]
-      @signed_info = @signature.elements["ds:SignedInfo"]
-      @reference = @signed_info.elements["ds:Reference"]
+      @signature = @element.at_xpath("ds:Signature", NS)
+      @signed_info = @signature.at_xpath("ds:SignedInfo", NS)
+      @reference = @signed_info.at_xpath("ds:Reference", NS)
     end
 
     def test_sign_inserts_after_issuer
-      children = @element.elements.to_a
+      children = @element.element_children
       issuer_index = children.index { |e| e.name == "Issuer" }
       signature_index = children.index { |e| e.name == "Signature" }
       assert_equal issuer_index + 1, signature_index
@@ -23,12 +25,12 @@ module Lyrebird
 
     def test_signature_element
       assert_equal "Signature", @signature.name
-      assert_equal "ds", @signature.prefix
-      assert_equal XMLDSIG_NS, @signature.namespace
+      assert_equal "ds", @signature.namespace.prefix
+      assert_equal XMLDSIG_NS, @signature.namespace.href
     end
 
     def test_signature_element_children
-      children = @signature.elements.to_a
+      children = @signature.element_children
       assert_equal 3, children.size
       assert_equal "SignedInfo", children[0].name
       assert_equal "SignatureValue", children[1].name
@@ -37,101 +39,123 @@ module Lyrebird
 
     def test_signed_info_element
       assert_equal "SignedInfo", @signed_info.name
-      assert_equal "ds", @signed_info.prefix
+      assert_equal "ds", @signed_info.namespace.prefix
     end
 
     def test_canonicalization_method
-      cm = @signed_info.elements["ds:CanonicalizationMethod"]
+      cm = @signed_info.at_xpath("ds:CanonicalizationMethod", NS)
       assert_equal "CanonicalizationMethod", cm.name
-      assert_equal EXC_C14N, cm.attributes["Algorithm"]
+      assert_equal EXC_C14N, cm["Algorithm"]
     end
 
     def test_signature_method
-      sm = @signed_info.elements["ds:SignatureMethod"]
+      sm = @signed_info.at_xpath("ds:SignatureMethod", NS)
       assert_equal "SignatureMethod", sm.name
-      assert_equal RSA_SHA256, sm.attributes["Algorithm"]
+      assert_equal RSA_SHA256, sm["Algorithm"]
     end
 
     def test_reference_element
       assert_equal "Reference", @reference.name
-      assert_equal "ds", @reference.prefix
+      assert_equal "ds", @reference.namespace.prefix
     end
 
     def test_reference_uri
-      element_id = @element.attributes["ID"]
-      assert_equal "##{element_id}", @reference.attributes["URI"]
+      element_id = @element["ID"]
+      assert_equal "##{element_id}", @reference["URI"]
     end
 
     def test_transforms_element
-      transforms = @reference.elements["ds:Transforms"]
+      transforms = @reference.at_xpath("ds:Transforms", NS)
       assert_equal "Transforms", transforms.name
-      assert_equal "ds", transforms.prefix
+      assert_equal "ds", transforms.namespace.prefix
     end
 
     def test_enveloped_signature_transform
-      transforms = @reference.elements["ds:Transforms"]
-      transform = transforms.elements.to_a("ds:Transform")[0]
-      assert_equal ENVELOPED_SIG, transform.attributes["Algorithm"]
+      transforms = @reference.xpath("ds:Transforms/ds:Transform", NS)
+      assert_equal ENVELOPED_SIG, transforms[0]["Algorithm"]
     end
 
     def test_c14n_transform
-      transforms = @reference.elements["ds:Transforms"]
-      transform = transforms.elements.to_a("ds:Transform")[1]
-      assert_equal EXC_C14N, transform.attributes["Algorithm"]
+      transforms = @reference.xpath("ds:Transforms/ds:Transform", NS)
+      assert_equal EXC_C14N, transforms[1]["Algorithm"]
     end
 
     def test_digest_method_element
-      digest_method = @reference.elements["ds:DigestMethod"]
+      digest_method = @reference.at_xpath("ds:DigestMethod", NS)
       assert_equal "DigestMethod", digest_method.name
-      assert_equal "ds", digest_method.prefix
+      assert_equal "ds", digest_method.namespace.prefix
     end
 
     def test_digest_method_algorithm
-      digest_method = @reference.elements["ds:DigestMethod"]
-      assert_equal SHA256_DIGEST, digest_method.attributes["Algorithm"]
+      digest_method = @reference.at_xpath("ds:DigestMethod", NS)
+      assert_equal SHA256_DIGEST, digest_method["Algorithm"]
     end
 
     def test_digest_value_element
-      digest_value = @reference.elements["ds:DigestValue"]
+      digest_value = @reference.at_xpath("ds:DigestValue", NS)
       assert_equal "DigestValue", digest_value.name
-      assert_equal "ds", digest_value.prefix
+      assert_equal "ds", digest_value.namespace.prefix
     end
 
     def test_digest_value_is_base64
-      digest_value = @reference.elements["ds:DigestValue"]
+      digest_value = @reference.at_xpath("ds:DigestValue", NS)
       decoded = Base64.strict_decode64(digest_value.text)
       assert_equal 32, decoded.bytesize
     end
 
     def test_signature_value_element
-      sv = @signature.elements["ds:SignatureValue"]
+      sv = @signature.at_xpath("ds:SignatureValue", NS)
       assert_equal "SignatureValue", sv.name
-      assert_equal "ds", sv.prefix
+      assert_equal "ds", sv.namespace.prefix
     end
 
     def test_signature_value_is_base64
-      sv = @signature.elements["ds:SignatureValue"]
+      sv = @signature.at_xpath("ds:SignatureValue", NS)
       decoded = Base64.strict_decode64(sv.text)
       assert_equal 256, decoded.bytesize
     end
 
     def test_key_info_element
-      ki = @signature.elements["ds:KeyInfo"]
+      ki = @signature.at_xpath("ds:KeyInfo", NS)
       assert_equal "KeyInfo", ki.name
-      assert_equal "ds", ki.prefix
+      assert_equal "ds", ki.namespace.prefix
     end
 
     def test_x509_data_element
-      x509_data = @signature.elements["ds:KeyInfo/ds:X509Data"]
+      x509_data = @signature.at_xpath("ds:KeyInfo/ds:X509Data", NS)
       assert_equal "X509Data", x509_data.name
-      assert_equal "ds", x509_data.prefix
+      assert_equal "ds", x509_data.namespace.prefix
     end
 
     def test_x509_certificate_element
-      cert = @signature.elements["ds:KeyInfo/ds:X509Data/ds:X509Certificate"]
+      xpath = "ds:KeyInfo/ds:X509Data/ds:X509Certificate"
+      cert = @signature.at_xpath(xpath, NS)
       assert_equal "X509Certificate", cert.name
-      assert_equal "ds", cert.prefix
+      assert_equal "ds", cert.namespace.prefix
       assert_equal @certificate.base64, cert.text
+    end
+
+    def test_digest_verifies
+      @signature.remove
+      c14n = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
+      canonical = @element.canonicalize(c14n)
+      computed = OpenSSL::Digest::SHA256.digest(canonical)
+
+      digest_value = @reference.at_xpath("ds:DigestValue", NS)
+      expected = Base64.strict_decode64(digest_value.text)
+      assert_equal expected, computed
+    end
+
+    def test_signature_verifies
+      c14n = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
+      canonical = @signed_info.canonicalize(c14n)
+
+      sig_value = @signature.at_xpath("ds:SignatureValue", NS)
+      signature_bytes = Base64.strict_decode64(sig_value.text)
+
+      public_key = @certificate.x509.public_key
+      verified = public_key.verify("SHA256", signature_bytes, canonical)
+      assert verified
     end
   end
 end
