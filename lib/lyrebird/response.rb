51153550da111d851ba7cbccf189108379421972
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-28 09:19:48 -0500

    Fix signature validation

--- lib/lyrebird/response.rb
+++ lib/lyrebird/response.rb
@@ -35,23 +35,25 @@ module Lyrebird
     end
 
     def mimic
-      Base64.strict_encode64(document.to_s)
+      Base64.strict_encode64(document.to_xml(save_with: 0))
     end
 
     def document
       Nokogiri::XML::Document.new.tap do |doc|
-        doc.root = root(doc)
+        doc.root = build_response(doc)
+        sign_assertion(doc) if @sign_with && !@encrypt_with
+        sign_response(doc) if @sign_with
       end
     end
 
     private
 
-    def root(doc)
+    def build_response(doc)
       doc.create_element("Response").tap do |r|
-        samlp = r.add_namespace_definition("samlp", SAML_PROTOCOL_NS)
-        saml = r.add_namespace_definition("saml", SAML_ASSERTION_NS)
+        @samlp = r.add_namespace_definition("samlp", SAML_PROTOCOL_NS)
+        @saml = r.add_namespace_definition("saml", SAML_ASSERTION_NS)
 
-        r.namespace = samlp
+        r.namespace = @samlp
         r["ID"] = ID.generate
         r["Version"] = "2.0"
         r["IssueInstant"] = Time.now.utc.iso8601
@@ -59,31 +61,44 @@ module Lyrebird
         r["InResponseTo"] = @in_response_to if @in_response_to
 
         issuer = doc.create_element("Issuer").tap do |i|
-          i.namespace = saml
+          i.namespace = @saml
           i.content = @issuer
         end
 
         r.add_child(issuer)
-        r.add_child(status(doc, samlp))
-        r.add_child(assertion_element)
-        Signature.new(r, @sign_with).sign! if @sign_with
+        r.add_child(build_status(doc))
+        r.add_child(build_assertion_element(doc))
       end
     end
 
-    def assertion_element
-      element = @assertion.document.root
-      Signature.new(element, @sign_with).sign! if @sign_with
-      return element unless @encrypt_with
+    def build_assertion_element(doc)
+      assertion_doc = @assertion.document
+      element = assertion_doc.root
 
-      Encryption.new(element, @encrypt_with).encrypt
+      if @encrypt_with
+        Signature.new(element, @sign_with).sign! if @sign_with
+        Encryption.new(element, @encrypt_with).encrypt
+      else
+        element
+      end
     end
 
-    def status(doc, ns)
+    def sign_assertion(doc)
+      ns = { "saml" => SAML_ASSERTION_NS }
+      assertion = doc.at_xpath("//saml:Assertion", ns)
+      Signature.new(assertion, @sign_with).sign!
+    end
+
+    def sign_response(doc)
+      Signature.new(doc.root, @sign_with).sign!
+    end
+
+    def build_status(doc)
       doc.create_element("Status").tap do |s|
-        s.namespace = ns
+        s.namespace = @samlp
 
         status_code = doc.create_element("StatusCode").tap do |sc|
-          sc.namespace = ns
+          sc.namespace = @samlp
           sc["Value"] = STATUS_SUCCESS
         end
 
