a9e40f8de52634df87aa7a85c92f85bc7149b995
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-01-28 09:06:58 -0500

    Convert Encryption to Nokogiri

--- lib/lyrebird/encryption.rb
+++ lib/lyrebird/encryption.rb
@@ -5,70 +5,97 @@ module Lyrebird
     def initialize(element, certificate)
       @element = element
       @certificate = certificate
+      @doc = element.document
       @aes_key = SecureRandom.random_bytes(32)
     end
 
     def encrypt
-      encrypted_assertion
+      build_encrypted_assertion
     end
 
     private
 
-    def encrypted_assertion
-      REXML::Element.new("saml:EncryptedAssertion").tap do |ea|
-        ea.add_namespace("saml", SAML_ASSERTION_NS)
-        ea.add_element(encrypted_data)
+    def build_encrypted_assertion
+      @doc.create_element("EncryptedAssertion").tap do |ea|
+        @saml = ea.add_namespace_definition("saml", SAML_ASSERTION_NS)
+        ea.namespace = @saml
+        ea.add_child(build_encrypted_data)
       end
     end
 
-    def encrypted_data
-      REXML::Element.new("xenc:EncryptedData").tap do |ed|
-        ed.add_namespace("xenc", XMLENC_NS)
-        ed.add_attribute("Type", "#{XMLENC_NS}Element")
-        em = ed.add_element("xenc:EncryptionMethod")
-        em.add_attribute("Algorithm", AES256_CBC)
-        ed.add_element(key_info)
-        ed.add_element(cipher_data)
+    def build_encrypted_data
+      @doc.create_element("EncryptedData").tap do |ed|
+        @xenc = ed.add_namespace_definition("xenc", XMLENC_NS)
+        ed.namespace = @xenc
+        ed["Type"] = "#{XMLENC_NS}Element"
+
+        enc_method = @doc.create_element("EncryptionMethod").tap do |em|
+          em.namespace = @xenc
+          em["Algorithm"] = AES256_CBC
+        end
+
+        ed.add_child(enc_method)
+        ed.add_child(build_key_info)
+        ed.add_child(build_cipher_data)
       end
     end
 
-    def key_info
-      REXML::Element.new("ds:KeyInfo").tap do |ki|
-        ki.add_namespace("ds", XMLDSIG_NS)
-        ki.add_element(encrypted_key)
+    def build_key_info
+      @doc.create_element("KeyInfo").tap do |ki|
+        @ds = ki.add_namespace_definition("ds", XMLDSIG_NS)
+        ki.namespace = @ds
+        ki.add_child(build_encrypted_key)
       end
     end
 
-    def encrypted_key
-      REXML::Element.new("xenc:EncryptedKey").tap do |ek|
-        ek.add_namespace("xenc", XMLENC_NS)
-        em = ek.add_element("xenc:EncryptionMethod")
-        em.add_attribute("Algorithm", RSA_OAEP)
-        ek.add_element(encrypted_key_cipher_data)
+    def build_encrypted_key
+      @doc.create_element("EncryptedKey").tap do |ek|
+        ek.add_namespace_definition("xenc", XMLENC_NS)
+        ek.namespace = @xenc
+
+        enc_method = @doc.create_element("EncryptionMethod").tap do |em|
+          em.namespace = @xenc
+          em["Algorithm"] = RSA_OAEP
+        end
+
+        ek.add_child(enc_method)
+        ek.add_child(build_encrypted_key_cipher_data)
       end
     end
 
-    def encrypted_key_cipher_data
+    def build_encrypted_key_cipher_data
       public_key = @certificate.x509.public_key
       padding = OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING
       encrypted_aes_key = public_key.public_encrypt(@aes_key, padding)
 
-      REXML::Element.new("xenc:CipherData").tap do |cd|
-        cv = Base64.strict_encode64(encrypted_aes_key)
-        cd.add_element("xenc:CipherValue").text = cv
+      @doc.create_element("CipherData").tap do |cd|
+        cd.namespace = @xenc
+
+        cipher_value = @doc.create_element("CipherValue").tap do |cv|
+          cv.namespace = @xenc
+          cv.content = Base64.strict_encode64(encrypted_aes_key)
+        end
+
+        cd.add_child(cipher_value)
       end
     end
 
-    def cipher_data
+    def build_cipher_data
       cipher = OpenSSL::Cipher.new("AES-256-CBC")
       cipher.encrypt
       cipher.key = @aes_key
       iv = cipher.random_iv
-      ciphertext = cipher.update(@element.to_s) + cipher.final
+      ciphertext = cipher.update(@element.to_xml) + cipher.final
 
-      REXML::Element.new("xenc:CipherData").tap do |cd|
-        cv = Base64.strict_encode64(iv + ciphertext)
-        cd.add_element("xenc:CipherValue").text = cv
+      @doc.create_element("CipherData").tap do |cd|
+        cd.namespace = @xenc
+
+        cipher_value = @doc.create_element("CipherValue").tap do |cv|
+          cv.namespace = @xenc
+          cv.content = Base64.strict_encode64(iv + ciphertext)
+        end
+
+        cd.add_child(cipher_value)
       end
     end
   end
--- test/lyrebird/encryption_test.rb
+++ test/lyrebird/encryption_test.rb
@@ -4,69 +4,75 @@ require "test_helper"
 
 module Lyrebird
   class EncryptionTest < Minitest::Test
+    NS = {
+      "saml" => SAML_ASSERTION_NS,
+      "xenc" => XMLENC_NS,
+      "ds" => XMLDSIG_NS
+    }.freeze
+
     def setup
       @assertion = Assertion.new.document
       @element = @assertion.root
       @certificate = Certificate.build
       @encrypted = Encryption.new(@element, @certificate).encrypt
-      @ed = @encrypted.elements["xenc:EncryptedData"]
-      @ki = @ed.elements["ds:KeyInfo"]
-      @ek = @ki.elements["xenc:EncryptedKey"]
-      @cd = @ed.elements["xenc:CipherData"]
+      @ed = @encrypted.at_xpath("xenc:EncryptedData", NS)
+      @ki = @ed.at_xpath("ds:KeyInfo", NS)
+      @ek = @ki.at_xpath("xenc:EncryptedKey", NS)
+      @cd = @ed.at_xpath("xenc:CipherData", NS)
     end
 
     def test_returns_encrypted_assertion_element
       assert_equal "EncryptedAssertion", @encrypted.name
-      assert_equal "saml", @encrypted.prefix
+      assert_equal "saml", @encrypted.namespace.prefix
     end
 
     def test_encrypted_assertion_namespace
-      assert_equal SAML_ASSERTION_NS, @encrypted.namespace
+      assert_equal SAML_ASSERTION_NS, @encrypted.namespace.href
     end
 
     def test_encrypted_data_element
       assert_equal "EncryptedData", @ed.name
-      assert_equal "xenc", @ed.prefix
+      assert_equal "xenc", @ed.namespace.prefix
     end
 
     def test_encrypted_data_namespace
-      assert_equal XMLENC_NS, @ed.namespace
+      assert_equal XMLENC_NS, @ed.namespace.href
     end
 
     def test_encrypted_data_type
-      assert_equal "#{XMLENC_NS}Element", @ed.attributes["Type"]
+      assert_equal "#{XMLENC_NS}Element", @ed["Type"]
     end
 
     def test_encryption_method_element
-      em = @ed.elements["xenc:EncryptionMethod"]
+      em = @ed.at_xpath("xenc:EncryptionMethod", NS)
       assert_equal "EncryptionMethod", em.name
-      assert_equal "xenc", em.prefix
+      assert_equal "xenc", em.namespace.prefix
     end
 
     def test_encryption_method_algorithm
-      em = @ed.elements["xenc:EncryptionMethod"]
-      assert_equal AES256_CBC, em.attributes["Algorithm"]
+      em = @ed.at_xpath("xenc:EncryptionMethod", NS)
+      assert_equal AES256_CBC, em["Algorithm"]
     end
 
     def test_cipher_data_element
       assert_equal "CipherData", @cd.name
-      assert_equal "xenc", @cd.prefix
+      assert_equal "xenc", @cd.namespace.prefix
     end
 
     def test_cipher_value_element
-      cv = @cd.elements["xenc:CipherValue"]
+      cv = @cd.at_xpath("xenc:CipherValue", NS)
       assert_equal "CipherValue", cv.name
-      assert_equal "xenc", cv.prefix
+      assert_equal "xenc", cv.namespace.prefix
     end
 
     def test_cipher_value_is_base64
-      cv = @cd.elements["xenc:CipherValue"]
+      cv = @cd.at_xpath("xenc:CipherValue", NS)
       decoded = Base64.strict_decode64(cv.text)
       assert decoded.bytesize > 16
     end
 
     def test_cipher_value_starts_with_iv
-      cv = @cd.elements["xenc:CipherValue"]
+      cv = @cd.at_xpath("xenc:CipherValue", NS)
       decoded = Base64.strict_decode64(cv.text)
       iv = decoded[0, 16]
       assert_equal 16, iv.bytesize
@@ -74,48 +80,48 @@ module Lyrebird
 
     def test_key_info_element
       assert_equal "KeyInfo", @ki.name
-      assert_equal "ds", @ki.prefix
+      assert_equal "ds", @ki.namespace.prefix
     end
 
     def test_key_info_namespace
-      assert_equal XMLDSIG_NS, @ki.namespace
+      assert_equal XMLDSIG_NS, @ki.namespace.href
     end
 
     def test_encrypted_key_element
       assert_equal "EncryptedKey", @ek.name
-      assert_equal "xenc", @ek.prefix
+      assert_equal "xenc", @ek.namespace.prefix
     end
 
     def test_encrypted_key_namespace
-      assert_equal XMLENC_NS, @ek.namespace
+      assert_equal XMLENC_NS, @ek.namespace.href
     end
 
     def test_encrypted_key_encryption_method
-      em = @ek.elements["xenc:EncryptionMethod"]
+      em = @ek.at_xpath("xenc:EncryptionMethod", NS)
       assert_equal "EncryptionMethod", em.name
-      assert_equal RSA_OAEP, em.attributes["Algorithm"]
+      assert_equal RSA_OAEP, em["Algorithm"]
     end
 
     def test_encrypted_key_cipher_data
-      cd = @ek.elements["xenc:CipherData"]
+      cd = @ek.at_xpath("xenc:CipherData", NS)
       assert_equal "CipherData", cd.name
-      assert_equal "xenc", cd.prefix
+      assert_equal "xenc", cd.namespace.prefix
     end
 
     def test_encrypted_key_cipher_value
-      cv = @ek.elements["xenc:CipherData/xenc:CipherValue"]
+      cv = @ek.at_xpath("xenc:CipherData/xenc:CipherValue", NS)
       assert_equal "CipherValue", cv.name
-      assert_equal "xenc", cv.prefix
+      assert_equal "xenc", cv.namespace.prefix
     end
 
     def test_encrypted_key_cipher_value_is_base64
-      cv = @ek.elements["xenc:CipherData/xenc:CipherValue"]
+      cv = @ek.at_xpath("xenc:CipherData/xenc:CipherValue", NS)
       decoded = Base64.strict_decode64(cv.text)
       assert decoded.bytesize > 0
     end
 
     def test_encrypted_key_can_be_decrypted
-      cv = @ek.elements["xenc:CipherData/xenc:CipherValue"]
+      cv = @ek.at_xpath("xenc:CipherData/xenc:CipherValue", NS)
       encrypted_key = Base64.strict_decode64(cv.text)
       private_key = @certificate.key
       padding = OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING
