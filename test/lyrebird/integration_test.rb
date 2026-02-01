0cab2665e2ec5c9699118113a62a7ea2b71b1a6f
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-02-01 16:54:28 -0500

    Add time-based validation integration tests

--- test/lyrebird/integration_test.rb
+++ test/lyrebird/integration_test.rb
@@ -132,5 +132,78 @@ module Lyrebird
       assert_equal groups, saml_response.attributes.multi("groups")
       assert_equal roles, saml_response.attributes.multi("roles")
     end
+
+    def test_ruby_saml_validates_custom_validity_period
+      name_id = "user@example.com"
+      not_before = Time.now.utc - 60
+      valid_for = 600
+
+      response = Response.build(sign_with: @idp_cert) do |r|
+        r.name_id = name_id
+        r.not_before = not_before
+        r.valid_for = valid_for
+      end
+
+      settings = OneLogin::RubySaml::Settings.new.tap do |s|
+        s.idp_cert = @idp_cert.x509_pem
+        s.sp_entity_id = DEFAULTS.audience
+        s.assertion_consumer_service_url = DEFAULTS.recipient
+      end
+
+      saml_response = OneLogin::RubySaml::Response.new(
+        response.mimic,
+        settings: settings
+      )
+
+      assert saml_response.is_valid?, saml_response.errors
+      assert_equal name_id, saml_response.nameid
+    end
+
+    def test_ruby_saml_rejects_expired_assertion
+      name_id = "user@example.com"
+
+      response = Response.build(sign_with: @idp_cert) do |r|
+        r.name_id = name_id
+        r.valid_for = -10
+      end
+
+      settings = OneLogin::RubySaml::Settings.new.tap do |s|
+        s.idp_cert = @idp_cert.x509_pem
+        s.sp_entity_id = DEFAULTS.audience
+        s.assertion_consumer_service_url = DEFAULTS.recipient
+      end
+
+      saml_response = OneLogin::RubySaml::Response.new(
+        response.mimic,
+        settings: settings
+      )
+
+      assert_operator Time.now.utc, :>=, saml_response.not_on_or_after
+      refute saml_response.is_valid?
+    end
+
+    def test_ruby_saml_rejects_not_yet_valid_assertion
+      name_id = "user@example.com"
+      not_before = Time.now.utc + 600
+
+      response = Response.build(sign_with: @idp_cert) do |r|
+        r.name_id = name_id
+        r.not_before = not_before
+      end
+
+      settings = OneLogin::RubySaml::Settings.new.tap do |s|
+        s.idp_cert = @idp_cert.x509_pem
+        s.sp_entity_id = DEFAULTS.audience
+        s.assertion_consumer_service_url = DEFAULTS.recipient
+      end
+
+      saml_response = OneLogin::RubySaml::Response.new(
+        response.mimic,
+        settings: settings
+      )
+
+      assert_operator Time.now.utc, :<, saml_response.not_before
+      refute saml_response.is_valid?
+    end
   end
 end
