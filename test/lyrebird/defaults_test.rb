cab2803f5a8cfa73dc5c9eb3b67cc79c5588bd6e
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-02-01 19:02:15 -0500

    Add tests for all default values

--- test/lyrebird/defaults_test.rb
+++ test/lyrebird/defaults_test.rb
@@ -7,5 +7,39 @@ module Lyrebird
     def test_issuer
       assert_equal "https://idp.example.com", DEFAULTS.issuer
     end
+
+    def test_name_id
+      assert_equal "user@example.com", DEFAULTS.name_id
+    end
+
+    def test_name_id_format
+      assert_equal NAMEID_EMAIL, DEFAULTS.name_id_format
+    end
+
+    def test_recipient
+      assert_equal "https://sp.example.com/acs", DEFAULTS.recipient
+    end
+
+    def test_in_response_to
+      assert_equal "_request_id", DEFAULTS.in_response_to
+    end
+
+    def test_valid_for
+      assert_equal 300, DEFAULTS.valid_for
+    end
+
+    def test_audience
+      assert_equal "https://sp.example.com", DEFAULTS.audience
+    end
+
+    def test_authn_context
+      expected = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+      assert_equal expected, DEFAULTS.authn_context
+    end
+
+    def test_attributes
+      expected = { first_name: "Test", last_name: "User" }
+      assert_equal expected, DEFAULTS.attributes
+    end
   end
 end
