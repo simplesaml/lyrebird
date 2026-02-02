28b49609983a8d38cb15b008a2642f31df581891
Josh <6605460+joshwetzel@users.noreply.github.com>
2026-02-01 19:05:49 -0500

    Add test for ID format

--- test/lyrebird/id_test.rb
+++ test/lyrebird/id_test.rb
@@ -7,5 +7,12 @@ module Lyrebird
     def test_generate_starts_with_underscore
       assert ID.generate.start_with?("_")
     end
+
+    def test_generate_contains_uuid
+      id = ID.generate
+      uuid = id[1..]
+      uuid_pattern = /\A[0-9a-f-]{36}\z/
+      assert_match uuid_pattern, uuid
+    end
   end
 end
